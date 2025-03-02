f502a6a21213c2c52bf7b6582c79971a1790e094
Liliana Gallegos <43852154+lilianaruva@users.noreply.github.com>
2025-03-02 01:36:29 -0600

    add nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import './index.scss'
 import Form from './components/form'
+import { IoMdAdd } from 'react-icons/io'
+import { MdEdit } from 'react-icons/md'
+import { useState } from 'react'
 
 function App() {
+  const [create, setCreate] =useState(true);
 
   return (
     <>
-      <Form />
+    <div className='nav'>
+      <button title='Editar' className={`${create ? '' : 'active'}`} onClick={() => setCreate(false)}><MdEdit /></button>
+      <button title='Crear' className={`${create ? 'active' : ''}`} onClick={() => setCreate(true)}><IoMdAdd /></button>
+    </div>
+    {
+      create ? <Form /> : <></>
+    }
     </>
   )
 }
