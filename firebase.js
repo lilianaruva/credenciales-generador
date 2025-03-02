cceaa2aeb41ade8d07ec4aaf63625827b9e5dbbe
Liliana Gallegos <43852154+lilianaruva@users.noreply.github.com>
2025-03-02 13:46:18 -0600

    add the fucking firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDUcsccilguum9jj07xUQ_n00XWMOhXdnQ",
+  authDomain: "crendenciales-amjub.firebaseapp.com",
+  projectId: "crendenciales-amjub",
+  storageBucket: "crendenciales-amjub.firebasestorage.app",
+  messagingSenderId: "726175179903",
+  appId: "1:726175179903:web:2ba9429d242b3e7d814b4a"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
